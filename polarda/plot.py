1792b0498b0a23caf1b8adce7e1f88acb6773b30
ErikHartman <erik.hartman@hotmail.com>
2023-08-31 15:49:29 +0200

    update

--- polarda/plot.py
+++ polarda/plot.py
@@ -18,32 +18,44 @@ class Plot:
 
 
 class PolarPlot:
-    def __init__(self, fig : matplotlib.figure.Figure, axs:  List[matplotlib.axes.Axes], values : np.ndarray, size:int=1, colors=None, cmap:str="coolwarm") -> None:
-
-        self.values = values,
+    def __init__(
+        self,
+        fig: matplotlib.figure.Figure,
+        ax: List[matplotlib.axes.Axes],
+        values: np.ndarray,
+        size: int = 1,
+        colors=None,
+        cmap: str = "coolwarm",
+    ) -> None:
+        self.values = values
         self.size = size
         self.colors = colors
         self.cmap = cmap
         self.fig = fig
-        self.axs = axs
+        self.axs = ax
 
-        if isinstance(axs, list):
-            axs = np.array(axs)
+
+        if isinstance(ax, list):
+            ax = np.array(ax)
         if not fig:
-            self.fig, self.axs = plt.subplots(1, 1, figsize=(5, 5))
+            self.fig= plt.figure(figsize=(5, 5))
+            self.ax = self.fig.add_subplot(projection='polar')
+            self.ax.grid(False)
+
 
     def plot(self):
-
         ngroups, npoints = self.values.shape
-        thetas = [n*2*np.pi/ngroups for n in range(ngroups)]
+
+        if self.colors is None:
+            self.colors = [1]*npoints
+        thetas = [n * 2 * np.pi / ngroups for n in range(ngroups)]
 
         magnitudes = []
         angles = []
 
         for point in range(npoints):
-            
             vals = self.values[:, point]
-            
+
             complexes = [get_complex(vals[i], thetas[i]) for i in range(ngroups)]
             real = sum([v.real for v in complexes])
             imag = sum([v.imag for v in complexes])
@@ -54,35 +66,51 @@ class PolarPlot:
 
             magnitudes.append(mag)
             angles.append(angle)
-        
-        self.ax.scatter(angles, magnitudes, s=self.size, c = self.colors, cmap=self.cmap, alpha=.9)
-        self.ax.vlines(thetas, 0, [max(magnitudes)]*ngroups, color="gray")
+
+        self.ax.scatter(
+            angles, magnitudes, s=self.size, c=self.colors, cmap=self.cmap, alpha=0.9
+        )
+        self.ax.vlines(thetas, 0, [max(magnitudes)] * ngroups, color="gray")
         if self.cmap:
-            cax = self.fig.add_axes([1,.1, 0.05,0.8])
-            plt.colorbar(matplotlib.cm.ScalarMappable(norm=matplotlib.colors.Normalize(vmin=min(self.colors), vmax=max(self.colors)), cmap=self.cmap), cax=cax)
-        
+            cax = self.fig.add_axes([1, 0.1, 0.05, 0.8])
+            plt.colorbar(
+                matplotlib.cm.ScalarMappable(
+                    norm=matplotlib.colors.Normalize(
+                        vmin=min(self.colors), vmax=max(self.colors)
+                    ),
+                    cmap=self.cmap,
+                ),
+                cax=cax,
+            )
+
         return self.fig, self.ax
 
     def rotate_labels(self, y_offset):
-        angles = np.linspace(0,2*np.pi,len(self.ax.get_xticklabels())+1)
+        angles = np.linspace(0, 2 * np.pi, len(self.ax.get_xticklabels()) + 1)
         angles[np.cos(angles) < 0] = angles[np.cos(angles) < 0] + np.pi
         angles = np.rad2deg(angles)
 
         labels = []
         for label, angle in zip(self.ax.get_xticklabels(), angles):
-            x,y = label.get_position()
-            lab = self.ax.text(x,y-y_offset, label.get_text(), transform=label.get_transform(),
-                        ha=label.get_ha(), va=label.get_va())
+            x, y = label.get_position()
+            lab = self.ax.text(
+                x,
+                y - y_offset,
+                label.get_text(),
+                transform=label.get_transform(),
+                ha=label.get_ha(),
+                va=label.get_va(),
+            )
             lab.set_rotation(angle)
             labels.append(lab)
         self.ax.set_xticklabels([])
 
 
-def get_angle(a,b):
+def get_angle(a, b):
     if a <= 0:
-        return np.pi + np.arctan(b/a)
-    return np.arctan(b/a)
+        return np.pi + np.arctan(b / a)
+    return np.arctan(b / a)
 
 
 def get_complex(magnitude, angle):
-    return magnitude*np.cos(angle) + magnitude*1j*np.sin(angle)
+    return magnitude * np.cos(angle) + magnitude * 1j * np.sin(angle)
